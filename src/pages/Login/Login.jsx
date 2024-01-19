47b8fa514c5d4387b3a153308518f64d79eea3f2
Tatiana Ivanova <tati.ivanova.87@gmail.com>
2024-01-20 00:15:16 +0300

    feat: add pop-up

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -41,52 +41,61 @@ function Login() {
   }, []);
 
   return (
-    <section className='flex justify-center items-center h-screen'>
-      <Form
-        layout='vertical'
-        className='w-400 bg-white p-2'
-        onFinish={onFinish}
-        form={form}
-      >
-        <h2 className='uppercase my-1 text-center text-primary'>
-          <span className='text-secondary'>Med</span>Connect Login
-        </h2>
-        <hr />
-        <Form.Item
-          label='Email'
-          name='email'
-          className='my-1'
-          rules={[
-            { required: true, message: 'Please, input your email!' },
-            { type: 'email', message: 'Please, input a valid email!' },
-          ]}
-          hasFeedback
+    <>
+      <section className='flex flex-column justify-center items-center h-screen'>
+        <Form
+          layout='vertical'
+          className='w-400 bg-white p-2'
+          onFinish={onFinish}
+          form={form}
         >
-          <Input type='email' />
-        </Form.Item>
+          <h2 className='uppercase my-1 text-center text-primary'>
+            <span className='text-secondary'>Med</span>Connect Login
+          </h2>
+          <hr />
+          <Form.Item
+            label='Email'
+            name='email'
+            className='my-1'
+            rules={[
+              { required: true, message: 'Please, input your email!' },
+              { type: 'email', message: 'Please, input a valid email!' },
+            ]}
+            hasFeedback
+          >
+            <Input type='email' />
+          </Form.Item>
 
-        <Form.Item
-          label='Password'
-          name={'password'}
-          rules={[{ required: true }]}
-          hasFeedback
+          <Form.Item
+            label='Password'
+            name={'password'}
+            rules={[{ required: true }]}
+            hasFeedback
+          >
+            <Input.Password />
+          </Form.Item>
+          <button
+            className='contained-btn my-1 w-full'
+            type='submit'
+          >
+            Login
+          </button>
+          <Link
+            className='underline link'
+            to='/register'
+          >
+            Don't have an account? <span className='bold'>Sign Up</span>
+          </Link>
+        </Form>
+        <p
+          data-tooltip='Email: admin@test.com
+          Password: Admin123###'
+          className='text-center mt-3'
         >
-          <Input.Password />
-        </Form.Item>
-        <button
-          className='contained-btn my-1 w-full'
-          type='submit'
-        >
-          Login
-        </button>
-        <Link
-          className='underline link'
-          to='/register'
-        >
-          Don't have an account? <span className='bold'>Sign Up</span>
-        </Link>
-      </Form>
-    </section>
+          To login as admin point here...
+        </p>
+      </section>
+    </>
   );
 }
 
